80c18913e7123fc75149860403314209360ec564
Asad Ali <asadali.code123@gmail.com>
2022-08-17 01:02:07 +0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from "next";
-import { useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { PieChart } from "react-minimal-pie-chart";
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { PieChart } from 'react-minimal-pie-chart'
 
 const Home: NextPage = () => {
   return (
     <div className="md:px-16 sm:px-1 2sm:px0">
       <div
         className={
-          styles["start-farming-widget"] +
-          " my-8 rounded-lg px-8 1sm:block py-5"
+          styles['start-farming-widget'] +
+          ' my-8 rounded-lg px-8 1sm:block py-5'
         }
       >
         <div className="flex items-center">
@@ -31,17 +31,17 @@ const Home: NextPage = () => {
           Start Farming
         </button>
       </div>
-      <div className="md:flex gap-4 flex-row my-8 sm:block 2sm:block">
+      <div className="md:flex gap-8 flex-row my-8 sm:block 2sm:block">
         <div className="net-pay-box basis-1/2 px-6 py-8 ">
           <span className=" small-label">Net Worth</span>
           <h3 className="">$981,312.51</h3>
         </div>
-        <div className=" net-pay-box basis-1/2 px-6 py-8 ">
+        <div className=" net-pay-box basis-1/2 px-6 pl-8 py-8 ">
           <span className="small-label">Net APY%</span>
           <h3>$981,312.51</h3>
         </div>
       </div>
-      <div className="md:flex flex-row px-16 py-8 rounded-lg my-8 sm:block 2sm:block">
+      <div className="md:flex flex-row px-6 py-8 rounded-lg my-8 sm:block 2sm:block">
         <div className="basis-1/2">
           <h5 className={styles.rowTitle}>Position Value Breakdown</h5>
           <div className={styles.lendingRow}>
@@ -65,7 +65,7 @@ const Home: NextPage = () => {
                   <div class="fill"></div>
                 </div>
                 <div class="inside-circle">
-                  {" "}
+                  {' '}
                   <Image
                     src="/icons/heart.svg"
                     alt="farming image"
@@ -80,7 +80,7 @@ const Home: NextPage = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
