c9075c4d321dae69d1bec43b91eead7c877b8c99
Asad Ali <asadali.code123@gmail.com>
2022-08-18 00:36:40 +0500

    bottom table fixed

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,19 +1,20 @@
 import { ReactNode } from "react";
-import Sidebar from './sidebar'
-import Header from './header'
-
+import Sidebar from "./sidebar";
+import Header from "./header";
+import styles from "./layout.module.scss";
 type LayoutProps = {
   children: NonNullable<ReactNode>;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  
   return (
-    <div className='min-h-screen bg-[#1E1E1E] flex flex-col md:flex-row flex-1'>
+    <div className="min-h-screen bg-[#1E1E1E] flex flex-col md:flex-row flex-1">
       <Sidebar />
-      <div className='flex bg-main flex-col flex-1'>
+      <div className="flex bg-main flex-col flex-1">
         <Header />
-        <main className=''>{children}</main>
+        <div>
+          <main className={styles.rightLayout}>{children}</main>
+        </div>
       </div>
     </div>
   );
