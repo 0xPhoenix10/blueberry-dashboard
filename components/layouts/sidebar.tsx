4628936320d81d3de44f15c35cc6a74a39a4f80a
Asad Ali <asadali.code123@gmail.com>
2022-08-19 01:15:13 +0500

    responsiveness

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -1,31 +1,31 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 const Sidebar = () => {
-  const router = useRouter();
+  const router = useRouter()
   const menuItems = [
     {
-      href: "/",
-      title: "Overview",
-      icon: "/icons/overview.svg",
+      href: '/',
+      title: 'Overview',
+      icon: '/icons/overview.svg',
     },
     {
-      href: "/earn",
-      title: "Earn",
-      icon: "/icons/earn.svg",
+      href: '/earn',
+      title: 'Earn',
+      icon: '/icons/earn.svg',
     },
     {
-      href: "/lend",
-      title: "Lend",
-      icon: "/icons/lend.svg",
+      href: '/lend',
+      title: 'Lend',
+      icon: '/icons/lend.svg',
     },
-  ];
+  ]
 
   return (
     <aside className="sidebar sticky top-0 max-h-screen bg-[#001223]">
       <div className="flex items-center justify-center h-[90px]">
-        <Link href={"/"}>
+        <Link href={'/'}>
           <a>
             <Image
               src="/icons/home.svg"
@@ -43,7 +43,7 @@ const Sidebar = () => {
               <Link href={href}>
                 <a
                   className={`flex p-2  justify-center rounded cursor-pointer ${
-                    router.asPath === href && "text-white"
+                    router.asPath === href && 'text-white'
                   }`}
                 >
                   <Image src={icon} alt={title} width={40} height={40} />
@@ -94,7 +94,7 @@ const Sidebar = () => {
         <button className='absolute bottom-4 p-2 bg-fuchsia-200 rounded hover:bg-fuchsia-400 cursor-pointer'>Link to everything</button>
       </div> */}
     </aside>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
