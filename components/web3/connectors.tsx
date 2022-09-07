a50570257f4f4af6c4c328df576bc4fbca84daa5
0xPhoenix9 <dev.aaron.k901@gmail.com>
2022-09-07 22:29:06 +0900

    wallet connect

--- components/web3/connectors.tsx
+++ components/web3/connectors.tsx
@@ -1,26 +1,28 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { InjectedConnector } from "@web3-react/injected-connector"
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
+import { WalletLinkConnector } from "@web3-react/walletlink-connector"
 
-const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
+const INFURA_ID = 'cde6e09b8a0742c59f07920306215aa4'
 
 const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
-const walletconnect = new WalletConnectConnector({
-  rpc: `https://mainnet.infura.io/v3/${INFURA_ID}`,
-  bridge: "https://bridge.walletconnect.org",
-  qrcode: true
-});
 
-const walletlink = new WalletLinkConnector({
+const WalletConnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+ });
+
+ const CoinbaseWallet = new WalletLinkConnector({
   url: `https://mainnet.infura.io/v3/${INFURA_ID}`,
-  appName: "web3-react-demo"
-});
+  appName: "Web3-react Demo",
+  supportedChainIds: [1, 3, 4, 5, 42],
+ });
 
 export const connectors = {
   injected: injected,
-  walletConnect: walletconnect,
-  coinbaseWallet: walletlink
+  walletConnect: WalletConnect,
+  coinbaseWallet: CoinbaseWallet
 };
